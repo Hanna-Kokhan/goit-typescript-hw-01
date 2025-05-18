fb84367dbd3efb51c8c0ce921a56d3032a438b2f
Hanna_Kokhan <anna.shkarupa2102@gmail.com>
2025-05-18 22:57:59 +0300

    modified 1.ts file

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -2,12 +2,12 @@ import axios, { AxiosResponse, AxiosError } from "axios";
 
 async function fetchData<T>(url: string): Promise<T> {
   try {
-    const response: AxiosResponse<T> = await axios.get<T>(url);
+    const response = await axios.get<T>(url);
     return response.data;
   } catch (error: any) {
     const axiosError = error as AxiosError;
     const errorMessage =
-      axiosError.message || error.message || "An unexpected error occurred";
+      axiosError?.message || error?.message || "An unexpected error occurred";
     throw new Error(`Error fetching from ${url}: ${errorMessage}`);
   }
 }
